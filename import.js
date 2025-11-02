e9cb73d66998ee4689548e7f3b307b12575bb9a5
Device Software <softwaredevice551@gmail.com>
2025-11-02 14:34:25 +0000

    Initial commit

--- import.js
+++ import.js
@@ -1,6 +1,7 @@
 const admin = require('firebase-admin');
 const csv = require('csv-parser');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 // Initialize Firebase Admin SDK
 const serviceAccount = require('./service-account.json');
@@ -11,69 +12,53 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
-const batchSize = 500;
-let documents = [];
-let rowCount = 0;
-let batchCount = 0;
+const documents = [];
 
-fs.createReadStream('pornhub.com-db.csv')
-  .pipe(csv({ separator: '|' }))
+function escapeCsvField(field) {
+  if (field === null || field === undefined) {
+    return null;
+  }
+  const stringField = String(field);
+  const needsQuotes = /[,"'\r\n]|<iframe/g.test(stringField);
+  if (needsQuotes) {
+    return `"${stringField.replace(/"/g, '""')}"`;
+  }
+  return stringField;
+}
+
+fs.createReadStream('simple.csv')
+  .pipe(csv({ 
+    separator: '|',
+    quote: '"',
+    escape: '\\'
+  }))
   .on('data', (row) => {
-    rowCount++;
+    const id = uuidv4();
 
-    // Convert tags to array
-    const tags = row.tags ? row.tags.split(',') : [];
-
-    // Create document data
     const docData = {
-      iframe_code: row.iframe_code || '',
-      thumbnail_url: row.thumbnail_url || '',
-      screenshots: row.screenshots || '',
-      title: row.title || '',
-      tags: tags,
-      categories: row.categories || '',
-      pornstars: row.pornstars || '',
-      duration: row.duration || '',
-      views: row.views || '',
-      rating: row.rating || '',
-      likes: row.likes || '',
-      dislikes: row.dislikes || '',
-      video_url: row.video_url || '',
-      thumbnail_urls: row.thumbnail_urls || '',
-    };
-
-    documents.push(docData);
-  })
-  .on('end', async () => {
-    const batchSize = 500;
-    let batch = db.batch();
-    let i = 0;
-
-    for (const docData of documents) {
-      const docRef = db.collection('videos').doc();
-      batch.set(docRef, docData);
-      i++;
-
-      if (i % batchSize === 0) {
-        batchCount++;
-        console.log(`Committing batch ${batchCount} with ${batchSize} rows`);
-        await batch.commit();
-        console.log(`Batch ${batchCount} committed successfully`);
-        batch = db.batch();
-      }
-    }
-
-    if (i % batchSize !== 0) {
-      batchCount++;
-      console.log(`Committing final batch ${batchCount} with ${i % batchSize} rows`);
-      await batch.commit();
-      console.log(`Final batch ${batchCount} committed successfully`);
-    }
-
-    console.log('CSV file successfully processed');
-    console.log(`Total rows processed: ${rowCount}`);
-    console.log(`Total batches committed: ${batchCount}`);
-  })
-  .on('error', (error) => {
-    console.error('Error reading CSV file: ', error);
-  });
+      id: id,
+      title: escapeCsvField(row["Gen Padova - Cum Bot Brunette Petite Girl Orgasms"]),
+      duration: escapeCsvField(row["185"]),
+      embed_url: escapeCsvField(row['<iframe src="https://www.pornhub.com/embed/f943adac0381c880e0ad" frameborder="0" height="481" width="608" scrolling="no"></iframe>|https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaf8GgaaaWavb)(mh=A_rM8VjTRqhkzV12)5.jpg']),
+      thumbnail_url: escapeCsvField(row["https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaf8GgaaaWavb)(mh=A_rM8VjTRqhkzV12)5.jpg"]),
+      image_urls: row["https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaf8GgaaaWavb)(mh=A_rM8VjTRqhkzV12)1.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaf8GgaaaWavb)(mh=A_rM8VjTRqhkzV12)2.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaf8GgaaaWavb)(mh=A_rM8VjTRqhkzV12)3.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaf8GgaaaWavb)(mh=A_rM8VjTRqhkzV12)4.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaf8GgaaaWavb)(mh=A_rM8VjTRqhkzV12)5.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaf8GgaaaWavb)(mh=A_rM8VjTRqhkzV12)6.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)7.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)8.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)9.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)10.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)11.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)12.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)13.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)14.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)15.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)16.jpg"] ? row["https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)1.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)2.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)3.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)4.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)5.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)6.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)7.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)8.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)9.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)10.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)11.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)12.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)13.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)14.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)15.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)16.jpg"].split(';') : null,
+50 |       tags: row["Big Tits;Blowjob;MILF;Pornstar;POV"] ? row["Big Tits;Blowjob;MILF;Pornstar;POV"].split(';') : null,
+51 |       views: row["2427054"] !== undefined ? parseInt(row["2427054"]) : 0,
+52 |       upvotes: row["929"] !== undefined ? parseInt(row["929"]) : 0,
+53 |       downvotes: row["310"] !== undefined ? parseInt(row["310"]) : 0,
+54 |       pornstar: escapeCsvField(row["Mandy May"] || null),
+55 |       categories: row["brazzers.com;big-tits;tittyfuck;pov;blowjob;handjob;cumshot;milf;czech"] ? row["brazzers.com;big-tits;tittyfuck;pov;blowjob;handjob;cumshot;milf;czech"].split(';') : null,
+56 |       thumbnail_url_2: escapeCsvField(row["https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)5.jpg"] || null),
+57 |       image_urls_2: row["https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)1.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)2.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)3.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)4.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)5.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)6.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)7.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)8.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)9.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)10.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)11.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)12.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)13.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)14.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)15.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)16.jpg"] ? row["https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)1.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)2.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)3.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)4.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)5.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)6.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)7.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)8.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)9.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)10.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)11.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)12.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)13.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)14.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)15.jpg;https://ei.phncdn.com/videos/200802/09/76478/original/(m=eaAaGwObaaamqv)(mh=pOlDv2oSsOP9h3SL)16.jpg"].split(';') : null
+58 |     };
+59 | 
+60 |     documents.push(docData);
+61 |   })
+62 |   .on('end', async () => {
+63 |     const jsonOutput = JSON.stringify(documents, null, 2);
+64 |     console.log(jsonOutput);
+65 |     fs.writeFileSync('data.json', jsonOutput, 'utf8');
+66 |   })
+67 |   .on('error', (error) => {
+68 |     console.error('Error reading CSV file: ', error);
+69 |   });
--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -25,55 +25,20 @@ const fs = require('fs');
 fs.createReadStream('simple.csv')
   .pipe(csv({ separator: '|' }))
   .on('data', (row: any) => {
-    Object.keys(row).forEach(key => {
-      if (row[key] === undefined) {
-        row[key] = '';
-      }
-    });
     db.collection('videos').add({
-      iframe_code: row.iframe_code || '',
-      thumbnail_url: row.thumbnail_url || '',
-      screenshots: row.screenshots || '',
-      title: row.title || '',
-      tags: row.tags || '',
-      categories: row.categories || '',
-      pornstars: row.pornstars || '',
-      duration: row.duration || '',
-      views: row.views || '',
-      rating: row.rating || '',
-      likes: row.likes || '',
-      dislikes: row.dislikes || '',
-      video_url: row.video_url || '',
-      thumbnail_urls: row.thumbnail_urls || '',
-    })
-    .then((docRef: any) => {
-      console.log('Document written with ID: ', docRef.id);
-    })
-    .catch((error: any) => {
-      console.error('Error adding document: ', error);
-    });
-  })
-  .on('data', (row: any) => {
-    Object.keys(row).forEach(key => {
-      if (row[key] === undefined) {
-        row[key] = '';
-      }
-    });
-    db.collection('videos').add({
-      iframe_code: row.iframe_code || '',
-      thumbnail_url: row.thumbnail_url || '',
-      screenshots: row.screenshots || '',
-      title: row.title || '',
-      tags: row.tags || '',
-      categories: row.categories || '',
-      pornstars: row.pornstars || '',
-      duration: row.duration || '',
-      views: row.views || '',
-      rating: row.rating || '',
-      likes: row.likes || '',
-      dislikes: row.dislikes || '',
-      video_url: row.video_url || '',
-      thumbnail_urls: row.thumbnail_urls || '',
+      embed_url: row['<iframe src="https://www.pornhub.com/embed/c3dbc9a5d726288d8a4b" frameborder="0" height="481" width="608" scrolling="no"></iframe>'],
+      thumbnail_url: row['https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaf8GgaaaWavb)(mh=VLBsRAsYYSS6491_)5.jpg'],
+      image_urls: row['https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaf8GgaaaWavb)(mh=VLBsRAsYYSS6491_)1.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaf8GgaaaWavb)(mh=VLBsRAsYYSS6491_)2.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaf8GgaaaWavb)(mh=VLBsRAsYYSS6491_)3.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaf8GgaaaWavb)(mh=VLBsRAsYYSS6491_)4.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaf8GgaaaWavb)(mh=VLBsRAsYYSS6491_)5.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaf8GgaaaWavb)(mh=VLBsRAsYYSS6491_)6.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaf8GgaaaWavb)(mh=VLBsRAsYYSS6491_)7.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaf8GgaaaWavb)(mh=VLBsRAsYYSS6491_)8.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaf8GgaaaWavb)(mh=VLBsRAsYYSS6491_)9.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaf8GgaaaWavb)(mh=VLBsRAsYYSS6491_)10.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaf8GgaaaWavb)(mh=VLBsRAsYYSS6491_)11.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaf8GgaaaWavb)(mh=VLBsRAsYYSS6491_)12.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaf8GgaaaWavb)(mh=VLBsRAsYYSS6491_)13.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaf8GgaaaWavb)(mh=VLBsRAsYYSS6491_)14.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaf8GgaaaWavb)(mh=VLBsRAsYYSS6491_)15.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaf8GgaaaWavb)(mh=VLBsRAsYYSS6491_)16.jpg'],
+      title: row['Gen Padova - Cum Bot Brunette Petite Girl Orgasms'],
+      categories: row['cumbots.com;machine;toys;brunette;teen;orgasm;natural-tits'],
+      tags: row['Brunette;Toys;Pornstar;18-25;Exclusive;Verified Models;Solo Female'],
+      pornstar: row['Gen Padova'],
+      duration: row['185'],
+      views: row['2346519'],
+      upvotes: row['3003'],
+      downvotes: row['415'],
+      thumbnail_url_2: row['https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaAaGwObaaamqv)(mh=3IROU0NPQStpTLN7)5.jpg'],
+      image_urls_2: row['https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaAaGwObaaamqv)(mh=3IROU0NPQStpTLN7)1.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaAaGwObaaamqv)(mh=3IROU0NPQStpTLN7)2.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaAaGwObaaamqv)(mh=3IROU0NPQStpTLN7)3.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaAaGwObaaamqv)(mh=3IROU0NPQStpTLN7)4.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaAaGwObaaamqv)(mh=3IROU0NPQStpTLN7)5.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaAaGwObaaamqv)(mh=3IROU0NPQStpTLN7)6.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaAaGwObaaamqv)(mh=3IROU0NPQStpTLN7)7.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaAaGwObaaamqv)(mh=3IROU0NPQStpTLN7)8.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaAaGwObaaamqv)(mh=3IROU0NPQStpTLN7)9.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaAaGwObaaamqv)(mh=3IROU0NPQStpTLN7)10.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaAaGwObaaamqv)(mh=3IROU0NPQStpTLN7)11.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaAaGwObaaamqv)(mh=3IROU0NPQStpTLN7)12.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaAaGwObaaamqv)(mh=3IROU0NPQStpTLN7)13.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaAaGwObaaamqv)(mh=3IROU0NPQStpTLN7)14.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaAaGwObaaamqv)(mh=3IROU0NPQStpTLN7)15.jpg;https://ei.phncdn.com/videos/200712/10/65404/original/(m=eaAaGwObaaamqv)(mh=3IROU0NPQStpTLN7)16.jpg']
     })
     .then((docRef: any) => {
       console.log('Document written with ID: ', docRef.id);
